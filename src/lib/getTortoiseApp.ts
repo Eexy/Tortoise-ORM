9df3985aed22370c1471257052df9936a52dc9f4
eexy <eexdhe@gmail.com>
2022-07-13 15:27:21 +0200

    docs: getTortoiseApp

--- src/lib/getTortoiseApp.ts
+++ src/lib/getTortoiseApp.ts
@@ -2,6 +2,12 @@ import firebase from "firebase/compat";
 import { getTortoiseStorage } from "./getTortoiseStorage";
 import App = firebase.app.App;
 
+/**
+ * Get app by name
+ * @param {string} appName
+ * @throws Throw error when app doesn't exist
+ * @return {App}
+ */
 export function getTortoiseApp(appName: string): App {
   const storage = getTortoiseStorage();
 
